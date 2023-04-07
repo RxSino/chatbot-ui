457a2a170a6da41013def3cb88223f09625186c8
稀加加 <905588195@qq.com>
2023-04-08 00:24:00 +0800

    ptg

--- pages/index.tsx
+++ pages/index.tsx
@@ -735,8 +735,8 @@ const Home: React.FC<HomeProps> = ({
   return (
     <>
       <Head>
-        <title>Chatbot UI</title>
-        <meta name="description" content="ChatGPT but better." />
+        <title>ChatTPG</title>
+        <meta name="description" content="ChatTPG" />
         <meta
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
